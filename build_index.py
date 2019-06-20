003a2f58d6d6a3c81b30a566408cc33839679859
T.J <thomas.turnage@noaa.gov>
2019-06-20 11:32:51 -0400

    Update build_index.py
    more comments

--- build_index.py
+++ build_index.py
@@ -9,8 +9,11 @@ import os
 import re
 import shutil
 import pathlib
+
+# pick directory with images to loop
 image_dir = 'C:/data/20080608/KDTX/images/mosaic/005'
-image_dir = 'C:/data/20190601/satellite/stage'
+#image_dir = 'C:/data/20190601/satellite/stage'
+
 im_dir = pathlib.PurePath(image_dir)
 e_date = im_dir.parts[2]
 if im_dir.parts[3] == 'satellite':
@@ -25,20 +28,25 @@ else:
     page_title = e_date + ' ' + e_radar + ' - ' + e_slice + ' deg'
     loop_description = 'Radar Velocity products derived with WDSS-II  (<a href="http://www.wdssii.org" target="_blank">http://www.wdssii.org/</a>)<br>'
 
+# following file has to be copied into image directory
 js_src = 'C:/data/scripts/resources/hanis_min.js'
-#image_dir = 'C:/data/20080608/KDTX/images/mosaic/005'
+
 shutil.copyfile(js_src,os.path.join(image_dir,'hanis_min.js'))
 
+# there will be an index.html file created in the image directory to be subsequently opened in an internet browser
 index_path = os.path.join(image_dir,'index.html')
 these_files = os.listdir(image_dir)
 
+# build list of image filenames
 file_str = ''
 for f in (these_files):
-    if re.search('png',f) is not None:
+    if (re.search('png',f) is not None) or (re.search('png',f) is not None):
         file_str = file_str + f + ', ' 
 
+#trim unwanted characters from string
 file_str = file_str[0:-2]
 
+# first part of html code
 html_1 = '<!doctype html>\
 <html>\
 <head>\
@@ -73,6 +81,7 @@ a:hover {\
 <body onload="HAniS.setup(\'filenames = '
 
 """
+Example of filenames list...
 ('filenames = 'HRRRMW_prec_radar_000.png, HRRRMW_prec_radar_001.png, HRRRMW_prec_radar_002.png, HRRRMW_prec_radar_003.png, HRRRMW_prec_radar_004.png, HRRRMW_prec_radar_005.png, HRRRMW_prec_radar_006.png, HRRRMW_prec_radar_007.png, HRRRMW_prec_radar_008.png, HRRRMW_prec_radar_009.png, HRRRMW_prec_radar_010.png, HRRRMW_prec_radar_011.png, HRRRMW_prec_radar_012.png, HRRRMW_prec_radar_013.png, HRRRMW_prec_radar_014.png, HRRRMW_prec_radar_015.png, HRRRMW_prec_radar_016.png, HRRRMW_prec_radar_017.png, HRRRMW_prec_radar_018.png\ncontrols = startstop, speed, step, looprock, zoom\ncontrols_style = display:flex;flex-flow:row;\nbuttons_style = flex:auto;margin:2px;cursor:pointer;\nbottom_controls = toggle\ntoggle_size = 8,8,2\ndwell = 100\npause = 1000','hanis')">
 """
 
